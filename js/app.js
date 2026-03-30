e4e6cc01baa0ae18f1a97042fe699c5500446f1a
tharanyj1-a11y <tharanyj1@gmail.com>
2026-03-30 19:45:01 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
+  if (currentQuestion >= 5) {
+    completeLevel();
+  } else {
+    loadQuestion();
+  }
+}
 
+function completeLevel() {
+  let level = localStorage.getItem("level");
+
+  let rewards = [
+    "Bitcoin", "Ethereum", "Litecoin", "Cardano",
+    "Solana", "Polkadot", "Chainlink",
+    "Uniswap", "Monero", "Tezos",
+    "Avalanche", "Polygon"
+  ];
+
+  alert("You earned " + rewards[level - 1]);
+
+  window.location = "dashboard.html";
+}
