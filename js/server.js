35888b2771cac368e3fcb8fb6de7fae47ce1c1e6
tharanyj1-a11y <tharanyj1@gmail.com>
2026-03-30 19:45:48 -0400

    Update server.js

--- js/server.js
+++ js/server.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const app = express();
 
+app.use(express.static("../frontend"));
+
+app.get("/backend/questions.json", (req, res) => {
+  res.sendFile(__dirname + "/questions.json");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
