325f2267c0337bfe0d93faf52e8afa387150964e
tharanyj1-a11y <tharanyj1@gmail.com>
2026-03-30 20:00:51 -0400

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,22 @@
+let username = localStorage.getItem("user");
+  let level = parseInt(localStorage.getItem("level"));
 
+  let rewards = [
+    "Bitcoin","Ethereum","Litecoin","Cardano",
+    "Solana","Polkadot","Chainlink","Uniswap",
+    "Monero","Tezos","Avalanche","Polygon"
+  ];
+
+  alert("You earned " + rewards[level - 1]);
+
+  fetch("/progress", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+      username: username,
+      level: level + 1
+    })
+  }).then(() => {
+    window.location = "dashboard.html";
+  });
+}
